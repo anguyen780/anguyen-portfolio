e1ca703e2acffa9b01f66a248e9558a01c91b922
Lux <anguyen780@gmail.com>
2022-11-27 19:52:00 -0600

    replaced contact me page with a mailto link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ function Navbar({currentPage, pageChange}) {
             <nav className='navbar-collapse justify-content-center'>
                 <a className='px-5 text-white nav-link about-link' href='#about-me' onClick={() => pageChange('About')}>About Me</a>
                 <a className='px-5 text-white nav-link port-link' href='#portfolio' onClick={() => pageChange('Portfolio')}>Portfolio</a>
-                <a className='px-5 text-white nav-link contact-link' href='#contact-me' onClick={() => pageChange('Contact')}>Contact Me</a>
+                <a className='px-5 text-white nav-link contact-link' href='mailto:anguyen780@gmail.com'>Contact Me</a>
                 <a className='px-5 text-white nav-link resume-link' href='https://docs.google.com/document/d/1AWhGWCMDSMDvummEZbdx_vNt2Mgr6fNgMNA21Keu_sI/edit?usp=sharing' target={'blank'}>Resume</a>
             </nav>
         </div>
