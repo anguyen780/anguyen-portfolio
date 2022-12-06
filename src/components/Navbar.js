43266849f9e31239b7081d9000a4112e62322edb
Lux <anguyen780@gmail.com>
2022-12-05 19:00:42 -0600

    updated resume

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,7 +9,7 @@ function Navbar({currentPage, pageChange}) {
                 <a className='px-5 text-white nav-link about-link' href='#about-me' onClick={() => pageChange('About')}>About Me</a>
                 <a className='px-5 text-white nav-link port-link' href='#portfolio' onClick={() => pageChange('Portfolio')}>Portfolio</a>
                 <a className='px-5 text-white nav-link contact-link' href='#contact-me' onClick={() => pageChange('Contact')}>Contact Me</a>
-                <a className='px-5 text-white nav-link resume-link' href='https://docs.google.com/document/d/1AWhGWCMDSMDvummEZbdx_vNt2Mgr6fNgMNA21Keu_sI/edit?usp=sharing' target={'blank'}>Resume</a>
+                <a className='px-5 text-white nav-link resume-link' href='https://docs.google.com/document/d/1xUcK6df5g5Yr0jknCxStE4hnpbcLNfY2sBPJ9jFrBG8/edit?usp=sharing' target={'blank'}>Resume</a>
             </nav>
         </div>
 
